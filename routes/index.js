5bd8b12d12bfbbfdc55ab1830fa2ced0471e9f7b
nddesigns <56118523+nikodimi@users.noreply.github.com>
2022-03-03 20:14:54 +0100

    Added controllers, models, and routes
    Added all necessary controllers, models and routes, based on example files,  without changing content.

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,8 @@ router.get('/', (req, res, next) => {
 });
 
 router.use('/example', require('./example'));
+router.use('/albums', require('./albums'));
+router.use('/photos', require('./photos'));
+router.use('/users', require('./users'));
 
 module.exports = router;
