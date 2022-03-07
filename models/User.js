afbb0f51f0b0d52d358436928df45c00de723f05
nddesigns <56118523+nikodimi@users.noreply.github.com>
2022-03-07 13:20:30 +0100

    Forgot to push old changes to user model

--- models/User.js
+++ models/User.js
@@ -5,5 +5,11 @@
 module.exports = (bookshelf) => {
 	return bookshelf.model('User', {
 		tableName: 'users',
+        photos() {
+            return this.hasMany('Photo')
+        },
+        albums() {
+            return this.hasMany('Album')
+        }
 	});
 };
