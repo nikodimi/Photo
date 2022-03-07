c1b130cabccf3a9cd2b3274d7adbaadb821e30eb
nddesigns <56118523+nikodimi@users.noreply.github.com>
2022-03-07 22:23:22 +0100

    Changed some validation rules

--- validation/album.js
+++ validation/album.js
@@ -9,14 +9,14 @@ const models = require('../models');
  * Create Album validation rules
  */
 const createRules = [
-	body('title').exists().isLength({ min: 4 }),
+	body('title').exists().isLength({ min: 3 }),
 ];
 
 /**
  * Update Album validation rules
  */
 const updateRules = [
-	body('title').optional().isLength({ min: 4 }),
+	body('title').exists().isLength({ min: 3 }),
 ];
 
 module.exports = {
--- validation/user.js
+++ validation/user.js
@@ -8,16 +8,16 @@ const models = require('../models');
  * Create the rules for user registration
  */
 const createRules = [
-    body('email').exists().isEmail().isLength({ min: 3 }).custom(async value => {
+    body('email').exists().isEmail().custom(async value => {
         const email = await new models.User({ email: value }).fetch({ require: false });
         if (email) {
             return Promise.reject('A user with this email already exists');
         }
         return Promise.resolve();
     }),
-    body('password').isLength({ min: 6 }),
-    body('first_name').isLength({ min: 3 }),
-    body('last_name').isLength({ min: 3 })
+    body('password').exists().isLength({ min: 6 }),
+    body('first_name').exists().isLength({ min: 3 }),
+    body('last_name').exists().isLength({ min: 3 })
 ];
 
 module.exports = {
